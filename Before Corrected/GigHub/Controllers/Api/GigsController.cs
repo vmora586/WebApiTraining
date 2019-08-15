37f15cc427d76f224dca29e8dfd201acfbe4882c
Victor Alfonso Mora Campiño <victor.mora@multienlace.com.co>
2019-08-15 16:29:45 -0500

    commit inicial para la extraccion de repositorios

---
+++ Before Corrected/GigHub/Controllers/Api/GigsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data.Entity;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+using GigHub.ViewModels;
+using GigHub.Repositories;
+
+namespace GigHub.Controllers.Api
+{
+    [RoutePrefix("api/gigs")]
+    public class GigsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly GicRepository _gigRepository;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+            _gigRepository = new GicRepository(_context);
+
+        }
+
+        [Route("{id:int}", Name = "GetGicById")]
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var gig = _gigRepository.GetGicById(id);
+
+            if (gig.IsCanceled)
+                return NotFound();
+
+            return Ok(gig);
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Create([FromBody]GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                //viewModel.Genres = _context.Genres.ToList();
+                return BadRequest();//the model sent is not valid
+            }
+
+            var gig = new Gig
+            {
+                ArtistId = "f4c311ce-31f1-4459-ab9d-edc647dd37fc",
+                DateTime = viewModel.GetDateTime(),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            var id = _gigRepository.AddGig(gig);
+
+            return Ok(id);
+            // return RedirectToAction("Mine", "Gigs");
+        }
+
+
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs
+                .Include(g => g.Attendances.Select(a => a.Attendee))
+                .Single(g => g.Id == id && g.ArtistId == userId);
+            
+            if (gig.IsCanceled)
+                return NotFound();
+
+            gig.Cancel();
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+       
+        
+
+        /*
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs
+                .Include(g => g.Attendances.Select(a => a.Attendee))
+                .Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+
+            gig.Modify(viewModel.GetDateTime(), viewModel.Venue, viewModel.Genre);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+        */
+
+    }
+}
---
+++ Before Corrected/GigHub/Repositories/GicRepository.cs
@@ -0,0 +1,41 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace GigHub.Repositories
+{
+    public class GicRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GicRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Gig GetGicById(int id)
+        {
+            /*
+              var gig = _context.Gigs
+                .Include(g => g.Attendances.Select(a => a.Attendee))
+                .Single(g => g.Id == id);
+             */
+
+            return _context.Gigs.Where(g => g.Id == id)
+           .Include(g => g.Attendances.Select(a => a.Attendee)).FirstOrDefault();
+        }
+
+        public int AddGig(Gig gig)
+        {
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return gig.Id;
+        }
+    }
+}
